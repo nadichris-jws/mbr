3f2d0a945581ccc3220aa3035cb06b690d0d06f2
nadichris-jws <nadi.chris@gmail.com>
2026-03-03 11:16:27 +0700

    Update data.js

--- data.js
+++ data.js
@@ -11,7 +11,7 @@ let sudahAdzan = {
   Isya:false
 }
 
-const lokasiId = 1605
+const lokasiId = 1602
 
 fetch(`https://api.myquran.com/v2/sholat/jadwal/${lokasiId}/${new Date().toISOString().slice(0,10).replace(/-/g,'/')}`)
 .then(r => r.json())
@@ -96,6 +96,7 @@ setInterval(() => {
   }
 
 }, 1000)
+
 
 
 
